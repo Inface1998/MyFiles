0e75d17b22f16e0a1a056126a02a841393d10019
inface <inface0443@gmail.com>
2021-12-30 11:07:18 +0800

    2021/12/30

--- Finish Version/AddAll.py
+++ Finish Version/AddAll.py
@@ -64,19 +64,19 @@ def create_fibers(arr, num):
             a.Instance(name='Part-fiber-{}'.format(num), part=part, dependent=ON)
             a.translate(instanceList=('Part-fiber-{}'.format(num),), vector=(x, y, z))
             a.rotate(instanceList=('Part-fiber-{}'.format(num),), axisPoint=(x, y, z),
-                              axisDirection=(0, 0, 1),
-                              angle=angle_x)
+                     axisDirection=(0, 0, 1),
+                     angle=angle_x)
             a.rotate(instanceList=('Part-fiber-{}'.format(num),), axisPoint=(x, y, z),
-                              axisDirection=(-1, np.tan((90 - (angle_x - 180)) * np.pi / 180), 0),
-                              angle=90 - angle_z)
+                     axisDirection=(-1, np.tan((90 - (angle_x - 180)) * np.pi / 180), 0),
+                     angle=90 - angle_z)
         if angle_x < 180:
             a.Instance(name='Part-fiber-{}'.format(num), part=part, dependent=ON)
             a.translate(instanceList=('Part-fiber-{}'.format(num),), vector=(x, y, z))
             a.rotate(instanceList=('Part-fiber-{}'.format(num),), axisPoint=(x, y, z),
-                              axisDirection=(0, 0, 1), angle=angle_x)
+                     axisDirection=(0, 0, 1), angle=angle_x)
             a.rotate(instanceList=('Part-fiber-{}'.format(num),), axisPoint=(x, y, z),
-                              axisDirection=(1, -np.tan((90 - angle_x) * np.pi / 180), 0),
-                              angle=90 - angle_z)
+                     axisDirection=(1, -np.tan((90 - angle_x) * np.pi / 180), 0),
+                     angle=90 - angle_z)
 
 
 # 骨料截面赋予
@@ -108,6 +108,36 @@ def create_tie(num):
                                   positionToleranceMethod=COMPUTED, adjust=ON, constraintEnforcement=SURFACE_TO_SURFACE)
 
 
+def set_fiber(n):
+    if n == 1:
+        return a.instances['Part-fiber-0'].edges[0:1]
+    else:
+        return a.instances['Part-fiber-{}'.format(n - 1)].edges[0:1] + set_fiber(n - 1)
+
+
+def set_aggregate(n):
+    if n == 1:
+        return a.instances['sphere0'].cells[0:1]
+    else:
+        return a.instances['sphere{}'.format(n-1)].cells[0:1] + set_aggregate(n - 1)
+
+
+def mesh_aggregate():
+    for i in range(countg):
+        p = mdb.models['Model-1'].parts['sphere{}'.format(i)]
+        session.viewports['Viewport: 1'].setValues(displayedObject=p)
+        elemType1 = mesh.ElemType(elemCode=DC3D8, elemLibrary=STANDARD)
+        elemType2 = mesh.ElemType(elemCode=DC3D6, elemLibrary=STANDARD)
+        elemType3 = mesh.ElemType(elemCode=DC3D4, elemLibrary=STANDARD)
+        c = p.cells
+        cells = c[0:1]
+        pickedRegions = (cells,)
+        p.setElementType(regions=pickedRegions, elemTypes=(elemType1, elemType2, elemType3))
+        p.seedPart(size=0.0075, deviationFactor=0.1, minSizeFactor=0.1)
+        p.setMeshControls(regions=c[0:1], elemShape=TET, technique=FREE)
+        p.generateMesh()
+
+
 side_length = 0.1  # mm,长方体混凝土边长
 # modeling
 myModel = mdb.Model(name='Model-1')
@@ -120,24 +150,27 @@ mdb.models['Model-1'].rootAssembly.Instance(dependent=ON, name='Part-Base-1',
 # 给基质骨料等赋予材料属性
 mdb.models['Model-1'].Material(name='Material-Matrix')
 mdb.models['Model-1'].materials['Material-Matrix'].Density(table=((2300.0,),))
-mdb.models['Model-1'].materials['Material-Matrix'].Elastic(table=((30e9, 0.3),))
-mdb.models['Model-1'].materials['Material-Matrix'].SpecificHeat(table=((1.0,),))
+mdb.models['Model-1'].materials['Material-Matrix'].Elastic(table=((40e9, 0.2),))
+mdb.models['Model-1'].materials['Material-Matrix'].SpecificHeat(table=((900,),))
 mdb.models['Model-1'].materials['Material-Matrix'].Expansion(table=((9.1e-06,),))
 mdb.models['Model-1'].materials['Material-Matrix'].Conductivity(table=((1.25,),))
+mdb.models['Model-1'].materials['Material-Matrix'].conductivity.setValues(
+    temperatureDependency=ON, table=((2.0, 20.0), (1.6, 300.0), (1.5, 400.0), (1.4, 500.0)))
 mdb.models['Model-1'].Material(name='Material-Aggregate')
 mdb.models['Model-1'].materials['Material-Aggregate'].Density(table=((2900.0,),))
-mdb.models['Model-1'].materials['Material-Aggregate'].Elastic(table=((40e9, 0.3),))
-mdb.models['Model-1'].materials['Material-Aggregate'].SpecificHeat(table=((1.2,),))
+mdb.models['Model-1'].materials['Material-Aggregate'].Elastic(table=((40e9, 0.2),))
+mdb.models['Model-1'].materials['Material-Aggregate'].SpecificHeat(table=((900,),))
 mdb.models['Model-1'].materials['Material-Aggregate'].Expansion(table=((8.1e-06,),))
 mdb.models['Model-1'].materials['Material-Aggregate'].Conductivity(table=((1.55,),))
 mdb.models['Model-1'].Material(name='Material-fiber')
 mdb.models['Model-1'].materials['Material-fiber'].Density(table=((7800,),))
 mdb.models['Model-1'].materials['Material-fiber'].Elastic(table=((200e9, 0.3),))
-mdb.models['Model-1'].materials['Material-fiber'].SpecificHeat(table=((1.0,),))
+mdb.models['Model-1'].materials['Material-fiber'].SpecificHeat(table=((500,),))
 mdb.models['Model-1'].materials['Material-fiber'].Expansion(table=((9.1e-06,),))
 mdb.models['Model-1'].materials['Material-fiber'].Conductivity(table=((1.25,),))
 # 生成立方体试块模型
 file_aggregate = '/Code/CycleInputFile/data_aggregate.txt'
+file_fibers = '/Code/CycleInputFile/data_fibers.txt'
 # 生成骨料
 a = mdb.models['Model-1'].rootAssembly
 bg = np.loadtxt(file_aggregate, delimiter=',', dtype=np.float32)
@@ -146,8 +179,8 @@ create_aggregate(bg, countg)
 cut_aggregate(countg)  # 切割基质，将基质中骨料部分挖除
 create_aggregate(bg, countg)  # 再次生产上一部中切除了骨料
 # 生成纤维
-bf = np.loadtxt('E:\Abaqus\Code\CycleInputFile\data_fibers.txt', delimiter=',',dtype=np.float32)
-countf = len(open(r"/Code/MyCoding\Code for abaqus\Finish Version\data_fibers.txt", 'rU').readlines())
+bf = np.loadtxt(file_fibers, delimiter=',', dtype=np.float32)
+countf = len(open(file_fibers, 'rU').readlines())
 FiberLength = bf[0][5]  # 读取纤维的长度
 part = mdb.models['Model-1'].Part(name="fibers", dimensionality=THREE_D, type=DEFORMABLE_BODY)
 sketch = mdb.models['Model-1'].ConstrainedSketch(name="sketch", sheetSize=200)
@@ -162,7 +195,7 @@ p = mdb.models['Model-1'].parts['Part-cut']
 cells = p.cells.findAt(((side_length, 0.666 * side_length, 0.666 * side_length),))
 region = regionToolset.Region(cells=cells)
 p.SectionAssignment(region=region, sectionName='Section-1', offset=0.0,
-                    offsetType=MIDDLE_SURFACE, offsetField='',thicknessAssignment=FROM_SECTION)
+                    offsetType=MIDDLE_SURFACE, offsetField='', thicknessAssignment=FROM_SECTION)
 # 纤维的截面属性赋予
 p = mdb.models['Model-1'].parts['fibers']
 edges = p.edges.findAt(((0.5 * FiberLength, 0.0, 0.0),))
@@ -171,7 +204,6 @@ p.SectionAssignment(region=region, sectionName='Section-2', offset=0.0,
                     offsetType=MIDDLE_SURFACE, offsetField='', thicknessAssignment=FROM_SECTION)
 # 骨料截面赋予
 assign_aggregate(countg)
-
 # 分析步设置
 mdb.models['Model-1'].HeatTransferStep(name='Step-1', previous='Initial', timePeriod=60, maxNumInc=100000,
                                        initialInc=0.01, minInc=1e-5, maxInc=10, deltmx=10, amplitude=STEP)
@@ -189,7 +221,7 @@ side1Faces1 = s1[0:6]
 region = a.Surface(side1Faces=side1Faces1, name='Surf-all')
 mdb.models['Model-1'].FilmCondition(name='Int-surChange',
                                     createStepName='Step-1', surface=region, definition=EMBEDDED_COEFF,
-                                    filmCoeff=1500, filmCoeffAmplitude='', sinkTemperature=1.0,
+                                    filmCoeff=12, filmCoeffAmplitude='', sinkTemperature=1.0,
                                     sinkAmplitude='Amp-1', sinkDistributionType=UNIFORM, sinkFieldName='')
 mdb.models['Model-1'].RadiationToAmbient(name='Int-surRadiation',
                                          createStepName='Step-1', surface=region, radiationType=AMBIENT,
@@ -197,25 +229,10 @@ mdb.models['Model-1'].RadiationToAmbient(name='Int-surRadiation',
                                          ambientTemperatureAmp='Amp-1')
 # 定义纤维集合
 a = mdb.models['Model-1'].rootAssembly
-fiberList = []
-edgesFibers = a.instances['Part-fiber-0'].edges[0:1]
-for i in range(countf):
-    fiberList.append(a.instances['Part-fiber-{}'.format(i)].edges[0:1])
-    if i > 0:
-        edgesFibers = edgesFibers + fiberList[i]
-    a.Set(edges=fiberList[i], name='Set-Fiber-{}'.format(i))
-
-
+edgesFibers = set_fiber(countf)
 # 定义骨料集合
-aggregateList = []
 cellsAggregate = a.instances['sphere0'].cells[0:1]
-for i in range(countg):
-    aggregateList.append(a.instances['sphere{}'.format(i)].cells[0:1])
-    if i > 0:
-        cellsAggregate = cellsAggregate + aggregateList[i]
-    a.Set(cells=aggregateList[i], name='Set-Aggregate-{}'.format(i))
-
-
+cellsAggregate = set_aggregate(countg)
 # 定义基质集合
 cellsMatrix = a.instances['Part-cut-1'].cells[0:1]
 a.Set(cells=cellsAggregate, name='Set-Aggregates')
@@ -246,7 +263,7 @@ pickedRegions = (c[0:1],)
 p.setElementType(regions=pickedRegions, elemTypes=(elemType1, elemType2, elemType3))
 pickedRegions = c[0:1]
 p.setMeshControls(regions=pickedRegions, elemShape=TET, technique=FREE)
-p.seedPart(size=0.01, deviationFactor=0.1, minSizeFactor=0.1)
+p.seedPart(size=0.00625, deviationFactor=0.1, minSizeFactor=0.1)
 p.generateMesh()
 # fiber mesh
 p = mdb.models['Model-1'].parts['fibers']
@@ -254,21 +271,10 @@ session.viewports['Viewport: 1'].setValues(displayedObject=p)
 elemType1 = mesh.ElemType(elemCode=DC1D2, elemLibrary=STANDARD)
 pickedRegions = (p.edges[0:1],)
 p.setElementType(regions=pickedRegions, elemTypes=(elemType1,))
-p.seedPart(size=0.0035, deviationFactor=0.1, minSizeFactor=0.1)
+p.seedPart(size=0.005, deviationFactor=0.1, minSizeFactor=0.1)
 p.generateMesh()
 # aggregate mesh
-for i in range(countg):
-    p = mdb.models['Model-1'].parts['sphere{}'.format(i)]
-    session.viewports['Viewport: 1'].setValues(displayedObject=p)
-    elemType1 = mesh.ElemType(elemCode=DC3D8, elemLibrary=STANDARD)
-    elemType2 = mesh.ElemType(elemCode=DC3D6, elemLibrary=STANDARD)
-    elemType3 = mesh.ElemType(elemCode=DC3D4, elemLibrary=STANDARD)
-    c = p.cells
-    cells = c[0:1]
-    pickedRegions = (cells,)
-    p.setElementType(regions=pickedRegions, elemTypes=(elemType1, elemType2, elemType3))
-    p.seedPart(size=0.005, deviationFactor=0.1, minSizeFactor=0.1)
-    p.setMeshControls(regions=c[0:1], elemShape=TET, technique=FREE)
-    p.generateMesh()
-mdb.saveAs(pathName='/Workpace/Model-temp01.cae')
+mesh_aggregate()
+a.regenerate()
+mdb.saveAs(pathName='/Workpace/Model-Temp01.cae')
 
